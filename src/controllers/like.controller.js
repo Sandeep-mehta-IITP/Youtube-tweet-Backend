94e3cc9f8dec9d70407402d6dcb7ab59e29effbf
Sandeep Mehta <sandeep797671@gmail.com>
2025-09-09 22:57:04 +0530

    Refactor like functionality: implement toggle logic for liking and disliking comments; enhance error handling for comment ID and user authentication.

--- src/controllers/like.controller.js
+++ src/controllers/like.controller.js
@@ -4,6 +4,7 @@ import { apiError } from "../utils/apiError.js";
 import { apiResponse } from "../utils/apiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { Video } from "../models/video.models.js";
+import { Comment } from "../models/comment.models.js";
 
 const toggleVideoLike = asyncHandler(async (req, res) => {
   const { videoId } = req.params;
@@ -33,13 +34,7 @@ const toggleVideoLike = asyncHandler(async (req, res) => {
 
     return res
       .status(200)
-      .json(
-        new apiResponse(
-          200, 
-          { isLiked: false }, 
-          "Disliked Successfully."
-        )
-      );
+      .json(new apiResponse(200, { isLiked: false }, "Video disliked Successfully."));
   }
 
   await Like.create({
@@ -49,18 +44,60 @@ const toggleVideoLike = asyncHandler(async (req, res) => {
 
   return res
     .status(200)
-    .json(
-      new apiResponse(
-        200, 
-        { isLiked: true }, 
-        "Liked successfully"
-      )
-    );
+    .json(new apiResponse(200, { isLiked: true }, "Video liked successfully"));
 });
 
 const toggleCommentLike = asyncHandler(async (req, res) => {
   const { commentId } = req.params;
   //TODO: toggle like on comment
+
+  if (!commentId) {
+    throw new apiError(400, "Comment ID is required.");
+  }
+
+  if (!req.user?._id) {
+    throw new apiError(401, "You must be logged in to like a comment.");
+  }
+
+  const comment = await Comment.findById(commentId);
+
+  if (!comment) {
+    throw new apiError(404, "Comment does not exist.");
+  }
+
+  const likedAlready = await Like.findOne({
+    comment: comment._id,
+    likedBy: req.user?._id,
+  }).lean();
+
+  if (likedAlready) {
+    await Like.findByIdAndDelete(likedAlready._id);
+
+    return res
+      .status(200)
+      .json(
+        new apiResponse(
+          200, 
+          { isLiked: false }, 
+          "Comment disliked successfully."
+        )
+      );
+  }
+
+  await Like.create({
+    comment: comment._id,
+    likedBy: req.user?._id,
+  });
+
+  return res
+    .status(200)
+    .json(
+      new apiResponse(
+        200, 
+        { isLiked: true }, 
+        "Comment liked successfully."
+      )
+    );
 });
 
 const toggleTweetLike = asyncHandler(async (req, res) => {
