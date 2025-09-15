34910c63b8634b5ffd2762a14ea50061cf289c8f
Sandeep Mehta <sandeep797671@gmail.com>
2025-09-15 13:16:18 +0530

    implement getChannelVideos: fetch videos uploaded by the channel with likes count and formatted date; enhance error handling for invalid user ID.

--- src/controllers/dashboard.controller.js
+++ src/controllers/dashboard.controller.js
@@ -14,7 +14,7 @@ const getChannelStats = asyncHandler(async (req, res) => {
     throw new apiError(400, "Invalid user ID.");
   }
 
-  // totalsubsribers -> channel id se 
+  // totalsubsribers -> channel id se
   const totalSubscribers = await Subscription.aggregate([
     {
       $match: {
@@ -72,7 +72,7 @@ const getChannelStats = asyncHandler(async (req, res) => {
     },
   ]);
 
-  // channel stats for response 
+  // channel stats for response
   const channelStats = {
     totalSubscribers: totalSubscribers[0]?.subscribersCount || 0,
     totalLikes: totalVideos[0]?.totalLikes || 0,
@@ -87,10 +87,73 @@ const getChannelStats = asyncHandler(async (req, res) => {
     );
 });
 
-
 // TODO: Get all the videos uploaded by the channel
 const getChannelVideos = asyncHandler(async (req, res) => {
-  
+  const userId = req.user?._id;
+
+  if (!isValidObjectId(userId)) {
+    throw new apiError(400, "Invalid user ID.");
+  }
+
+  const channelVideos = await Video.aggregate([
+    {
+      $match: {
+        owner: new mongoose.Types.ObjectId(userId),
+      },
+    },
+    {
+      $lookup: {
+        from: "likes",
+        localField: "_id",
+        foreignField: "video",
+        as: "likes",
+      },
+    },
+    {
+      $sort: {
+        createdAt: -1,
+      },
+    },
+    {
+      $addFields: {
+        createdAtParts: {
+          $dateToParts: {
+            date: "$createdAt",
+          },
+        },
+        likesCount: {
+          $size: "$likes",
+        },
+      },
+    },
+    {
+      $project: {
+        _id: 1,
+        videoFile: 1,
+        thumbnail: 1,
+        title: 1,
+        description: 1,
+        views: 1,
+        likesCount: 1,
+        isPublished: 1,
+        formattedDate: {
+          date: "$createdAtParts.day",
+          month: "$createdAtParts.month",
+          year: "$createdAtParts.year",
+        },
+      },
+    },
+  ]);
+
+  return res
+    .status(200)
+    .json(
+      new apiResponse(
+        200,
+        channelVideos,
+        "Channel videos fetched successfully."
+      )
+    );
 });
 
 export { getChannelStats, getChannelVideos };
