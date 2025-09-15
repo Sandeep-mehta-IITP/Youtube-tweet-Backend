e9ea71b1fe2fe6751570dae8b9b517bb42837287
Sandeep Mehta <sandeep797671@gmail.com>
2025-09-15 12:23:39 +0530

    implement healthcheck response with status, uptime, and timestamp; improve error handling for failures.

--- src/controllers/healthcheck.controller.js
+++ src/controllers/healthcheck.controller.js
@@ -2,8 +2,25 @@ import { apiError } from "../utils/apiError.js";
 import { apiResponse } from "../utils/apiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 
+//TODO: build a healthcheck response that simply returns the OK status as json with a message
 const healthcheck = asyncHandler(async (req, res) => {
-  //TODO: build a healthcheck response that simply returns the OK status as json with a message
+  try {
+    return res
+      .status(200)
+      .json(
+        new apiResponse(
+          200,
+          {
+            message: "Everything is OKAY",
+            uptime: process.uptime(),
+            timestamp: new Date(),
+          },
+          "Okay"
+        )
+      );
+  } catch (error) {
+    throw new apiError(503, "Healthcheck failed", error.message);
+  }
 });
 
 export { healthcheck };
