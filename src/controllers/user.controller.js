9f880696838a63c595773d17fd8d36c81c402ced
Sandeep Mehta <sandeep797671@gmail.com>
2025-11-12 10:52:05 +0530

    Remove resetPasswordToken and resetPasswordExpiry from user data selection in registerUser and loginUser for enhanced security

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -121,7 +121,7 @@ const registerUser = asyncHandler(async (req, res) => {
   });
 
   const createdUser = await User.findById(user._id).select(
-    "-password -refreshToken"
+    "-password -refreshToken -resetPasswordToken -resetPasswordExpiry"
   );
 
   if (!createdUser) {
@@ -181,7 +181,7 @@ const loginUser = asyncHandler(async (req, res) => {
   );
 
   const loggedInUser = await User.findById(user._id).select(
-    "-password -refreshToken"
+    "-password -refreshToken -resetPasswordToken -resetPasswordExpiry"
   );
 
   const options = {
